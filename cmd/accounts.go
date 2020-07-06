44e2c16ddc48c49cdf237d2a11877b0f44adfda9
Isaiah-Turner <42742035+Isaiah-Turner@users.noreply.github.com>
2020-07-06 18:45:19 -0400

    Fixed wording of export_account command and its related comments

--- cmd/accounts.go
+++ cmd/accounts.go
@@ -6,16 +6,16 @@ import (
 
 // accountsCmd represents the accounts command
 var accountsCmd = &cobra.Command{
-	Use:   "accounts",
+	Use:   "export_accounts",
 	Short: "Exports the account data.",
-	Long:  `Exports historical account data within the specified ledger range to an output file.`,
+	Long:  `Exports historical account data within the specified range to an output file.`,
 	Run: func(cmd *cobra.Command, args []string) {
 		/*
 			Functionality planning:
 			1. Read in start and end ledger numbers
 			2. Provide ledger number to ingestion system and receive data
-			3. Convert data from ingestion into an array of Ledger structs with length equal to the limit
-			4. Write array to output file
+			3. Convert data from ingestion into an slice of Account structs (length should at most be the limit)
+			4. Write slice to output file
 			TODO: Consider having a way to export directly to BigQuery using bigquery library
 		*/
 	},
@@ -33,7 +33,9 @@ func init() {
 			start-ledger: the ledger sequence number for the beginning of the export period
 			end-ledger: the ledger sequence number for the end of the export range (required)
 
-			limit: maximum number of accounts to export; default to 1000
+			limit: maximum number of accounts to export;
+				TODO: measure a good default value that ensures all accounts within a 5 minute period will be exported with a single call
+
 			output-file: filename of the output file
 	*/
 }
