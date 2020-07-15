afe2aab1d539a2a815bca60da5c7878cf87d1e96
Isaiah-Turner <42742035+Isaiah-Turner@users.noreply.github.com>
2020-07-15 16:34:48 -0400

    Created a test that comprehensively tests against a hardcoded ledger

--- internal/transform/ledger_test.go
+++ internal/transform/ledger_test.go
@@ -1,68 +1,17 @@
 package transform
 
 import (
-	"errors"
 	"fmt"
 	"testing"
 	"time"
 
 	"github.com/stellar/stellar-etl/internal/utils"
 
-	"github.com/gdexlab/go-render/render"
-
-	"github.com/stellar/go/exp/ingest/ledgerbackend"
 	"github.com/stellar/go/keypair"
 	"github.com/stellar/go/txnbuild"
 	"github.com/stellar/go/xdr"
 	"github.com/stretchr/testify/assert"
 )
-
-func createTestArchiveBackend() *ledgerbackend.HistoryArchiveBackend {
-	archiveStellarURL := "http://history.stellar.org/prd/core-live/core_live_001"
-	backend, err := ledgerbackend.NewHistoryArchiveBackendFromURL(archiveStellarURL)
-	utils.PanicOnError(err)
-	return backend
-}
-func TestTransformOnTrueLedger(t *testing.T) {
-	sequenceNumber := uint32(30511378)
-	backend := createTestArchiveBackend()
-	defer backend.Close()
-
-	ok, testLedger, err := backend.GetLedger(sequenceNumber)
-	utils.PanicOnError(err)
-	if !ok {
-		panic("Ledger does not exist")
-	}
-	encodedLedgerHeader := "AAAADQV9uRTeVDxeos9csdoGFP4EVPC7fjwdREAdvbJGK3Mdlf53swrayxtj+WanhAsXUxXGrlWjA24P1WYTEHqDNscAAAAAXwXKJgAAAAAAAAAAfA3y3e25icQxUH/+uuK927qF7CA80UGOyyxaUZXw30NQfVdKEsLSPrKyXtQDHS9r2bBx0JYnj5m9aELaByqf4gHRkRIOoh6z7HlbYQAAEII3mx+pAAABFgAAAAAPCrxaAAAAZABMS0AAAAPob1lZHuPT2yeYW7x4Zs+KV0kGxc28wQEbbCIVQefXPPozBfmto7Eki4cSFF1t4R7PTUInwbXia1D32UdH49065HgU8x0XGBQSpM2IDZnned+TWHn8dogc71zlLocnXFhMTCIWC0Qiq8Pzem7s3VeReXI7rAo/U1ze1hS2OJ3voNkAAAAA"
-	byteFormOfLedgerHeader := []byte(encodedLedgerHeader)
-	parsedTime, err := time.Parse("2006-1-2 15:04:05 MST", "2020-07-08 13:29:10 UTC")
-	desiredOutput := LedgerOutput{
-		Sequence:           int32(sequenceNumber),
-		LedgerHash:         "6898f0f7b4945f47152aa697eaf79b3df7ddaa7d1a3c697e28197ad1feab7b0e",
-		PreviousLedgerHash: "057db914de543c5ea2cf5cb1da0614fe0454f0bb7e3c1d44401dbdb2462b731d",
-		LedgerHeader:       byteFormOfLedgerHeader,
-
-		TransactionCount:           10,
-		OperationCount:             12,
-		SuccessfulTransactionCount: 10,
-		FailedTransactionCount:     1,
-		TxSetOperationCount:        "13",
-
-		ClosedAt: parsedTime,
-
-		TotalCoins:      1054439020873472865,
-		FeePool:         18151464705961,
-		BaseFee:         100,
-		BaseReserve:     5000000,
-		MaxTxSetSize:    1000,
-		ProtocolVersion: 13,
-	}
-	inputLedgerPretty := render.AsCode(testLedger)
-	fmt.Println(inputLedgerPretty)
-	convertedLedger, err := TransformLedger(testLedger)
-	assert.Equal(t, desiredOutput, convertedLedger)
-
-}
 
 func TestTransformLedger(t *testing.T) {
 	type transformTest struct {
@@ -70,46 +19,68 @@ func TestTransformLedger(t *testing.T) {
 		wantOutput LedgerOutput
 		wantErr    error
 	}
-	/*encodedLedgerHeader := "AAAADfY8FdDq9Ir711GkxN+t5Uo0SAU8R8WnHWImaK4MwqIIkvQSArXhiHcF4mc5dW+oX7J45SX+kVIw1fZFQOgAGUEAAAAAXwtt4wAAAAAAAAAAglgbxuZna3ZZPDE9cmMZZu4hr7EKrE9oMaGRPu2QqnhEVa9EjsMjpKJnOr9UnDpix+BSese+YEEnMYBllAoQYAHSmSUOoh6z7HlbYQAAEILAyUaJAAABFgAAAAAPNsPXAAAAZABMS0AAAAPomTlchfG4kTZ/6o0otCLK44Twu6KISQjni2rUp4dG8r+uRv1ZyQAYHHxS1EDGrx4+4hJAXwwfQZIiQ02F7J/5e/MWwpi1ImmLz+kwObksbcGQNVo7wwrC65BaoDpSpMXAeBTzHRcYFBKkzYgNmed535NYefx2iBzvXOUuhydcWEwAAAAA"
-	byteFormOfLedgerHeader := []byte(encodedLedgerHeader)
-	parsedTime, err := time.Parse("2006-1-2 15:04:05 MST", "2020-07-12 20:09:07 UTC")*/
+	hardCodedTxSet := xdr.TransactionSet{
+		Txs: []xdr.TransactionEnvelope{
+			createSampleTx(0),
+			createSampleTx(1),
+		},
+	}
+	hardCodedTxProcessing := []xdr.TransactionResultMeta{
+		createSampleResultMeta(false, 3),
+		createSampleResultMeta(true, 10),
+	}
+	hardCodedLedger, _ := xdr.NewLedgerCloseMeta(0, xdr.LedgerCloseMetaV0{
+		LedgerHeader: xdr.LedgerHeaderHistoryEntry{
+			Header: xdr.LedgerHeader{
+				LedgerSeq:          30578981,
+				TotalCoins:         1054439020873472865,
+				FeePool:            18153766209161,
+				BaseFee:            100,
+				BaseReserve:        5000000,
+				MaxTxSetSize:       1000,
+				LedgerVersion:      13,
+				PreviousLedgerHash: xdr.Hash{0xf6, 0x3c, 0x15, 0xd0, 0xea, 0xf4, 0x8a, 0xfb, 0xd7, 0x51, 0xa4, 0xc4, 0xdf, 0xad, 0xe5, 0x4a, 0x34, 0x48, 0x5, 0x3c, 0x47, 0xc5, 0xa7, 0x1d, 0x62, 0x26, 0x68, 0xae, 0xc, 0xc2, 0xa2, 0x8},
+				ScpValue:           xdr.StellarValue{CloseTime: 1594584547},
+			},
+			Hash: xdr.Hash{0x26, 0x93, 0x2d, 0xc4, 0xd8, 0x4b, 0x5f, 0xab, 0xe9, 0xae, 0x74, 0x4c, 0xb4, 0x3c, 0xe4, 0xc6, 0xda, 0xcc, 0xf9, 0x8c, 0x86, 0xa9, 0x91, 0xb2, 0xa1, 0x49, 0x45, 0xb1, 0xad, 0xac, 0x4d, 0x59},
+		},
+		TxSet:        hardCodedTxSet,
+		TxProcessing: hardCodedTxProcessing,
+	})
+	correctTime, err := time.Parse("2006-1-2 15:04:05 MST", "2020-07-12 20:09:07 UTC")
+	utils.PanicOnError(err)
+	correctHeader, _ := xdr.MarshalBase64(hardCodedLedger.MustV0().LedgerHeader.Header)
+	correctBytes := []byte(correctHeader)
 	tests := []transformTest{
 		{
 			wrapLedgerHeader(xdr.LedgerHeader{
 				TotalCoins: -1,
 			}),
 			LedgerOutput{},
-			errors.New("The total number of coins is a negative value"),
+			fmt.Errorf("The total number of coins (-1) is negative for ledger 0"),
 		},
 		{
 			wrapLedgerHeader(xdr.LedgerHeader{
 				FeePool: -1,
 			}),
 			LedgerOutput{},
-			errors.New("The fee pool is a negative value"),
+			fmt.Errorf("The fee pool (-1) is negative for ledger 0"),
 		},
 		{
 			wrapLedgerHeaderWithTransactions(xdr.LedgerHeader{
 				MaxTxSetSize: 0,
 			}, 2),
 			LedgerOutput{},
-			errors.New("The number of transactions and results are different"),
+			fmt.Errorf("The number of transactions and results are different (2 != 0)"),
 		},
-		/*{
-
+		{
+			hardCodedLedger,
 			LedgerOutput{
 				Sequence:           int32(30578981),
 				LedgerHash:         "26932dc4d84b5fabe9ae744cb43ce4c6daccf98c86a991b2a14945b1adac4d59",
 				PreviousLedgerHash: "f63c15d0eaf48afbd751a4c4dfade54a3448053c47c5a71d622668ae0cc2a208",
-				LedgerHeader:       byteFormOfLedgerHeader,
-
-				TransactionCount:           2,
-				OperationCount:             6,
-				SuccessfulTransactionCount: 2,
-				FailedTransactionCount:     201,
-				TxSetOperationCount:        "208",
-
-				ClosedAt: parsedTime,
+				LedgerHeader:       correctBytes,
+				ClosedAt:           correctTime,
 
 				TotalCoins:      1054439020873472865,
 				FeePool:         18153766209161,
@@ -117,9 +88,15 @@ func TestTransformLedger(t *testing.T) {
 				BaseReserve:     5000000,
 				MaxTxSetSize:    1000,
 				ProtocolVersion: 13,
+
+				TransactionCount:           1,
+				OperationCount:             10,
+				SuccessfulTransactionCount: 1,
+				FailedTransactionCount:     1,
+				TxSetOperationCount:        "13",
 			},
 			nil,
-		}*/
+		},
 	}
 
 	for _, test := range tests {
@@ -175,4 +152,28 @@ func createSampleTx(sequence uint32) xdr.TransactionEnvelope {
 	env, err := tx.TxEnvelope()
 	utils.PanicOnError(err)
 	return env
+}
+
+func createSampleResultMeta(successful bool, subOperationCount int) xdr.TransactionResultMeta {
+	resultCode := xdr.TransactionResultCodeTxFailed
+	if successful {
+		resultCode = xdr.TransactionResultCodeTxSuccess
+	}
+	operationResults := []xdr.OperationResult{}
+	for i := 0; i < subOperationCount; i++ {
+		operationResults = append(operationResults, xdr.OperationResult{
+			Code: xdr.OperationResultCodeOpInner,
+			Tr:   &xdr.OperationResultTr{},
+		})
+	}
+	return xdr.TransactionResultMeta{
+		Result: xdr.TransactionResultPair{
+			Result: xdr.TransactionResult{
+				Result: xdr.TransactionResultResult{
+					Code:    resultCode,
+					Results: &operationResults,
+				},
+			},
+		},
+	}
 }
