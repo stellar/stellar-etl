3af298be320adcefc5e9daefdb8ac0889b751800
Isaiah-Turner <42742035+Isaiah-Turner@users.noreply.github.com>
2020-08-10 16:03:16 -0400

    Removed captive core tests

--- cmd/export_accounts_test.go
+++ cmd/export_accounts_test.go
@@ -18,30 +18,6 @@ func TestExportAccounts(t *testing.T) {
 			golden:  "bucket_read_off.golden",
 			wantErr: nil,
 		},
-		{
-			name:    "accounts from one ledger",
-			args:    []string{"export_accounts", "-s", "10820015", "-e", "10820015", "--stdout", "-x", "../stellar-core/src/stellar-core", "-c", "../stellar-core/docs/stellar-core_example.cfg"},
-			golden:  "one_ledger_accs.golden",
-			wantErr: nil,
-		},
-		{
-			name:    "accounts from 10 ledgers",
-			args:    []string{"export_accounts", "-s", "17736172", "-e", "17736182", "--stdout", "-x", "../stellar-core/src/stellar-core", "-c", "../stellar-core/docs/stellar-core_example.cfg"},
-			golden:  "10_ledgers_accs.golden",
-			wantErr: nil,
-		},
-		{
-			name:    "range too large",
-			args:    []string{"export_accounts", "-s", "10820000", "-e", "10820015", "-l", "5", "--stdout", "-x", "../stellar-core/src/stellar-core", "-c", "../stellar-core/docs/stellar-core_example.cfg"},
-			golden:  "large_range_accs.golden",
-			wantErr: nil,
-		},
-		{
-			name:    "ledger with no accounts",
-			args:    []string{"export_accounts", "-s", "2", "-e", "2", "--stdout", "-x", "../stellar-core/src/stellar-core", "-c", "../stellar-core/docs/stellar-core_example.cfg"},
-			golden:  "ledger_no_accs.golden",
-			wantErr: nil,
-		},
 	}
 
 	for _, test := range tests {
