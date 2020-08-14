582eefff4f14d5bb4bff938c981caedceae090de
Isaiah-Turner <42742035+Isaiah-Turner@users.noreply.github.com>
2020-08-14 14:26:26 -0400

    Changed streaming so it streams in batches

--- internal/input/changes.go
+++ internal/input/changes.go
@@ -63,23 +63,6 @@ func PrepareCaptiveCore(execPath, configPath string, start, end uint32) (*ledger
 	return captiveBackend, nil
 }
 
-// getCompactedChanges compacts the read changes from a ledger and returns them
-func getCompactedChanges(changeReader *ingestio.LedgerChangeReader) []ingestio.Change {
-	changeCache := ingestio.NewLedgerEntryChangeCache()
-	for {
-		change, err := changeReader.Read()
-		if err == ingestio.EOF {
-			break
-		}
-
-		if err == nil {
-			changeCache.AddChange(change)
-		}
-	}
-
-	return changeCache.GetChanges()
-}
-
 // sendToChannel sends a ledgerentry to the appropriate channel, checking that the channel is not nil before sending
 func sendToChannel(entry xdr.LedgerEntry, accChannel, offChannel, trustChannel chan xdr.LedgerEntry) {
 	switch entry.Data.Type {
@@ -117,31 +100,76 @@ func closeChannels(accChannel, offChannel, trustChannel chan xdr.LedgerEntry) {
 }
 
 // sendLedgerChangesToChannels streams the changes from a single ledger to the provided channels
-func sendLedgerChangesToChannels(seqNum uint32, core *ledgerbackend.CaptiveStellarCore, accChannel, offChannel, trustChannel chan xdr.LedgerEntry) {
-	changeReader, err := ingestio.NewLedgerChangeReader(core, network.PublicNetworkPassphrase, seqNum)
-	if err == nil {
-		compactedChanges := getCompactedChanges(changeReader)
-		for _, change := range compactedChanges {
-			// TODO: figure out how to handle deleted entries
-			if change.Post != nil {
-				entry := *change.Post
-				sendToChannel(entry, accChannel, offChannel, trustChannel)
-			}
+func sendChangesToChannels(compactedChanges []ingestio.Change, accChannel, offChannel, trustChannel chan xdr.LedgerEntry) {
+	for _, change := range compactedChanges {
+		// TODO: figure out how to handle deleted entries
+		if change.Post != nil {
+			entry := *change.Post
+			sendToChannel(entry, accChannel, offChannel, trustChannel)
 		}
 	}
 }
 
+func addLedgerChangesToCache(changeReader *ingestio.LedgerChangeReader, changeCache *ingestio.LedgerEntryChangeCache) error {
+	for {
+		change, err := changeReader.Read()
+		if err == ingestio.EOF {
+			break
+		}
+
+		if err != nil {
+			return err
+		}
+
+		changeCache.AddChange(change)
+	}
+	return nil
+}
+
+// exportBatch gets the changes from the ledgers in the range [batchStart, batchEnd), compacts them, and sends them to the proper channels
+func exportBatch(batchStart, batchEnd uint32, core *ledgerbackend.CaptiveStellarCore, accChannel, offChannel, trustChannel chan xdr.LedgerEntry, logger *log.Entry) {
+	batchChanges := ingestio.NewLedgerEntryChangeCache()
+
+	for seq := batchStart; seq < batchEnd; seq++ {
+		changeReader, err := ingestio.NewLedgerChangeReader(core, network.PublicNetworkPassphrase, seq)
+		if err != nil {
+			logger.Error(fmt.Sprintf("unable to create change reader for ledger %d", seq), err)
+		}
+
+		err = addLedgerChangesToCache(changeReader, batchChanges)
+		if err != nil {
+			logger.Error(fmt.Sprintf("unable to read changes from ledger %d", seq), err)
+		}
+
+		changeReader.Close()
+	}
+
+	compactedChanges := batchChanges.GetChanges()
+	sendChangesToChannels(compactedChanges, accChannel, offChannel, trustChannel)
+}
+
 // StreamChanges runs a goroutine that reads in ledgers, processes the changes, and send the changes to the channel matching their type
-func StreamChanges(core *ledgerbackend.CaptiveStellarCore, start, end uint32, accChannel, offChannel, trustChannel chan xdr.LedgerEntry) {
+func StreamChanges(core *ledgerbackend.CaptiveStellarCore, start, end, batchSize uint32, accChannel, offChannel, trustChannel chan xdr.LedgerEntry, logger *log.Entry) {
+
 	if end != 0 {
-		for seq := start; seq <= end; seq++ {
-			sendLedgerChangesToChannels(seq, core, accChannel, offChannel, trustChannel)
+		totalBatches := uint32((end-start+1)/batchSize + 1)
+		for currentBatch := uint32(0); currentBatch < totalBatches; currentBatch++ {
+			batchStart := start + currentBatch*batchSize
+			batchEnd := batchStart + batchSize
+			if batchEnd > end+1 {
+				batchEnd = end + 1
+			}
+
+			exportBatch(batchStart, batchEnd, core, accChannel, offChannel, trustChannel, logger)
 		}
 	} else {
-		currentLedger := start
+		batchStart := start
+		batchEnd := batchStart + batchSize
 		for {
-			sendLedgerChangesToChannels(currentLedger, core, accChannel, offChannel, trustChannel)
-			currentLedger++
+			//TODO: figure out what happens when an unbounded batch goes further than the
+			exportBatch(batchStart, batchEnd, core, accChannel, offChannel, trustChannel, logger)
+			batchStart = batchEnd
+			batchEnd = batchStart + batchSize
 		}
 	}
 
