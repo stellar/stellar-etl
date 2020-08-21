829b993211f15dfc4bed9470c6d38a1b8adf17dd
Isaiah-Turner <42742035+Isaiah-Turner@users.noreply.github.com>
2020-08-21 16:05:07 -0400

    stellar-etl: set up time range to ledger range cmd (#90)
    * Created command file and added flags
    
    * Added function header for getting range

---
+++ internal/input/ledger_range.go
@@ -0,0 +1,9 @@
+package input
+
+import "time"
+
+// GetLedgerRange calculates the ledger range that spans the provided date range
+func GetLedgerRange(startTime, endTime time.Time) (uint32, uint32, error) {
+	// TODO: implement this function
+	return 0, 0, nil
+}
