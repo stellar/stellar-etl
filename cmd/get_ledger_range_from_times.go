ff56a36496ddbca325e6c2ff435860588b228cac
Isaiah-Turner <42742035+Isaiah-Turner@users.noreply.github.com>
2020-08-24 11:46:10 -0400

    Improved command description

--- cmd/get_ledger_range_from_times.go
+++ cmd/get_ledger_range_from_times.go
@@ -15,14 +15,16 @@ var getLedgerRangeFromTimesCmd = &cobra.Command{
 	Use:   "get_ledger_range_from_times",
 	Short: "Converts a time range into a ledger range",
 	Long: `Converts a time range into a ledger range. Times must be in the format YYYY-MM-DDTHH:MM:SS.SSSZ.
-	Some examples include: 2006-01-02T15:04:05-0700, 2009-11-10T18:00:00-0500, or 2019-09-13T23:00:00+0000`,
+	Some examples include: 2006-01-02T15:04:05-0700, 2009-11-10T18:00:00-0500, or 2019-09-13T23:00:00+0000.
+	If the time range goes into the future, the ledger range will end on the most recent ledger. If the time
+	range covers time before the network started, the ledger range will start with the genesis ledger.`,
 	Run: func(cmd *cobra.Command, args []string) {
 		startString, err := cmd.Flags().GetString("start-time")
 		if err != nil {
 			cmdLogger.Fatal("could not get start time: ", err)
 		}
 
-		endString, err := cmd.Flags().GetString("start-time")
+		endString, err := cmd.Flags().GetString("end-time")
 		if err != nil {
 			cmdLogger.Fatal("could not get end time: ", err)
 		}
@@ -50,9 +52,9 @@ var getLedgerRangeFromTimesCmd = &cobra.Command{
 		}
 
 		if !useStdout {
-			outFile.WriteString(fmt.Sprintf("%d %d", startLedger, endLedger))
+			outFile.WriteString(fmt.Sprintf("%d %d\n", startLedger, endLedger))
 		} else {
-			fmt.Printf("%d %d", startLedger, endLedger)
+			fmt.Printf("%d %d\n", startLedger, endLedger)
 		}
 	},
 }
