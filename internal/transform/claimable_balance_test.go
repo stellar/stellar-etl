6f6047b6a856d1717bc3f6c6712d2e4c6ce27ae4
erika-sdf <erika@stellar.org>
2021-12-14 12:15:13 -0800

    Add claimable balances to ledger changes.

---
+++ internal/transform/claimable_balance_test.go
@@ -0,0 +1,116 @@
+package transform
+
+import (
+	"database/sql"
+	"github.com/guregu/null"
+	"github.com/stellar/go/ingest"
+	"github.com/stellar/go/xdr"
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestTransformClaimableBalance(t *testing.T) {
+	type transformTest struct {
+		input      ingest.Change
+		wantOutput ClaimableBalanceOutput
+		wantErr    error
+	}
+
+	input := makeClaimableBalanceTestInput()
+	output := makeClaimableBalanceTestOutput()
+
+	tests := []transformTest{
+		{
+			input,
+			output, nil,
+		},
+	}
+
+	for _, test := range tests {
+		actualOutput, actualError := TransformClaimableBalance(test.input)
+		assert.Equal(t, test.wantErr, actualError)
+		assert.Equal(t, test.wantOutput, actualOutput)
+	}
+}
+
+func makeClaimableBalanceTestInput() ingest.Change {
+	ledgerEntry := xdr.LedgerEntry{
+		Ext: xdr.LedgerEntryExt{
+			V: 1,
+			V1: &xdr.LedgerEntryExtensionV1{
+				SponsoringId: &xdr.AccountId{
+					Type:    0,
+					Ed25519: &xdr.Uint256{1, 2, 3, 4, 5, 6, 7, 8, 9},
+				},
+				Ext: xdr.LedgerEntryExtensionV1Ext{
+					V: 1,
+				},
+			},
+		},
+		LastModifiedLedgerSeq: 30705278,
+		Data: xdr.LedgerEntryData{
+			Type: xdr.LedgerEntryTypeClaimableBalance,
+			ClaimableBalance: &xdr.ClaimableBalanceEntry{
+				BalanceId: genericClaimableBalance,
+				Claimants: []xdr.Claimant{
+					{
+						Type: 0,
+						V0: &xdr.ClaimantV0{
+							Destination: testAccount1ID,
+						},
+					},
+				},
+				Asset: xdr.Asset{
+					Type: xdr.AssetTypeAssetTypeCreditAlphanum12,
+					AlphaNum12: &xdr.AlphaNum12{
+						AssetCode: xdr.AssetCode12{1,2,3,4,5,6,7,8,9},
+						Issuer:    testAccount3ID,
+					},
+				},
+				Amount: 9990000000,
+				Ext: xdr.ClaimableBalanceEntryExt{
+					V: 1,
+					V1: &xdr.ClaimableBalanceEntryExtensionV1{
+						Ext: xdr.ClaimableBalanceEntryExtensionV1Ext{
+							V: 1,
+						},
+						Flags: 10,
+					},
+				},
+			},
+		},
+	}
+	return ingest.Change{
+		Type: xdr.LedgerEntryTypeClaimableBalance,
+		Pre:  &ledgerEntry,
+		Post: nil,
+	}
+}
+
+func makeClaimableBalanceTestOutput() ClaimableBalanceOutput {
+	return ClaimableBalanceOutput{
+		BalanceID: "0102030405060708090000000000000000000000000000000000000000000000",
+		Claimants: []Claimant{
+			{
+				Destination: "GCEODJVUUVYVFD5KT4TOEDTMXQ76OPFOQC2EMYYMLPXQCUVPOB6XRWPQ",
+				Predicate: xdr.ClaimPredicate{
+					Type: xdr.ClaimPredicateTypeClaimPredicateUnconditional,
+				},
+			},
+		},
+		AssetIssuer: "GBT4YAEGJQ5YSFUMNKX6BPBUOCPNAIOFAVZOF6MIME2CECBMEIUXFZZN",
+		AssetType:   "credit_alphanum12",
+		AssetCode:   "\x01\x02\x03\x04\x05\x06\a\b\t",
+		AssetAmount:      999,
+		Sponsor: null.String{
+			NullString: sql.NullString{
+				String: "GAAQEAYEAUDAOCAJAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAABO3W",
+				Valid:  true,
+			},
+		},
+		Flags:              10,
+		LastModifiedLedger: 30705278,
+		LedgerEntryChange:  2,
+		Deleted:            true,
+	}
+}
