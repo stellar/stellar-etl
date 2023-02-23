89a30a66481bdab8fab2d8a2c260c720c0b0ba87
lucas zanotelli <lucas@zanotelli.dev>
2023-02-23 15:34:42 -0300

    update test to accommodate backend changes

--- cmd/export_trades_test.go
+++ cmd/export_trades_test.go
@@ -6,27 +6,28 @@ import (
 
 func TestExportTrades(t *testing.T) {
 	tests := []cliTest{
+		// TODO: change ledger range to mirror test name as soon as official backend is provisioned
 		{
 			name:    "trades from one ledger",
-			args:    []string{"export_trades", "-s", "28770265", "-e", "28770265", "-o", gotTestDir(t, "one_ledger_trades.txt")},
+			args:    []string{"export_trades", "--testnet", "--gcs-bucket", "not", "-s", "1410048", "-e", "1410048", gotTestDir(t, "one_ledger_trades.txt")},
 			golden:  "one_ledger_trades.golden",
 			wantErr: nil,
 		},
 		{
 			name:    "trades from 10 ledgers",
-			args:    []string{"export_trades", "-s", "28770265", "-e", "28770275", "-o", gotTestDir(t, "10_ledgers_trades.txt")},
+			args:    []string{"export_trades", "--testnet", "--gcs-bucket", "not", "-s", "1410100", "-e", "1410110", "-o", gotTestDir(t, "10_ledgers_trades.txt")},
 			golden:  "10_ledgers_trades.golden",
 			wantErr: nil,
 		},
 		{
 			name:    "range too large",
-			args:    []string{"export_trades", "-s", "28770265", "-e", "28770275", "-l", "5", "-o", gotTestDir(t, "large_range_trades.txt")},
+			args:    []string{"export_trades", "--testnet", "--gcs-bucket", "not", "-s", "1410100", "-e", "1410110", "-l", "5", "-o", gotTestDir(t, "large_range_trades.txt")},
 			golden:  "large_range_trades.golden",
 			wantErr: nil,
 		},
 		{
 			name:    "ledger with no trades",
-			args:    []string{"export_trades", "-s", "10363513", "-e", "10363513", "-o", gotTestDir(t, "ledger_no_trades.txt")},
+			args:    []string{"export_trades", "--testnet", "--gcs-bucket", "not", "-s", "1410048", "-e", "1410048", gotTestDir(t, "ledger_no_trades.txt")},
 			golden:  "ledger_no_trades.golden",
 			wantErr: nil,
 		},
