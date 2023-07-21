42b70a7f5fdaf793bf0331b4a89ff8ca452c5d1c
sydneynotthecity <sydney@stellar.org>
2023-07-21 10:09:34 -0500

    Finish unit tests for assets

--- internal/transform/asset_test.go
+++ internal/transform/asset_test.go
@@ -6,37 +6,25 @@ import (
 
 	"github.com/stretchr/testify/assert"
 
+	"github.com/stellar/go/ingest"
 	"github.com/stellar/go/xdr"
 )
 
 func TestTransformAsset(t *testing.T) {
-	// type operationInput struct {
-	// 	operation   xdr.Operation
-	// 	index       int32
-	// 	transaction ingest.LedgerTransaction
-	// }
+
 	type assetInput struct {
 		operation xdr.Operation
 		index     int32
 		txnIndex  int32
 		// transaction xdr.TransactionEnvelope
 	}
-	// type transformTest struct {
-	// 	input      operationInput
-	// 	wantOutput AssetOutput
-	// 	wantErr    error
-	// }
+
 	type transformTest struct {
 		input      assetInput
 		wantOutput AssetOutput
 		wantErr    error
 	}
 
-	// nonPaymentInput := operationInput{
-	// 	operation:   genericBumpOperation,
-	// 	transaction: genericLedgerTransaction,
-	// 	index:       0,
-	// }
 	nonPaymentInput := assetInput{
 		operation: genericBumpOperation,
 		txnIndex:  0,
@@ -51,17 +39,20 @@ func TestTransformAsset(t *testing.T) {
 		},
 	}
 
-	// hardCodedInputTransaction, err := makeAssetTestInput()
-	// assert.NoError(t, err)
-	// hardCodedOutputArray := makeAssetTestOutput()
+	hardCodedInputTransaction, err := makeAssetTestInput()
+	assert.NoError(t, err)
+	hardCodedOutputArray := makeAssetTestOutput()
 
-	// for i, op := range hardCodedInputTransaction.Envelope.Operations() {
-	// 	tests = append(tests, transformTest{
-	// 		input:      operationInput{op, int32(i), hardCodedInputTransaction},
-	// 		wantOutput: hardCodedOutputArray[i],
-	// 		wantErr:    nil,
-	// 	})
-	// }
+	for i, op := range hardCodedInputTransaction.Envelope.Operations() {
+		tests = append(tests, transformTest{
+			input: assetInput{
+				operation: op,
+				index:     int32(i),
+				txnIndex:  int32(i)},
+			wantOutput: hardCodedOutputArray[i],
+			wantErr:    nil,
+		})
+	}
 
 	for _, test := range tests {
 		actualOutput, actualError := TransformAsset(test.input.operation, test.input.index, test.input.txnIndex, 0)
@@ -70,58 +61,58 @@ func TestTransformAsset(t *testing.T) {
 	}
 }
 
-// func makeAssetTestInput() (inputTransaction ingest.LedgerTransaction, err error) {
-// 	inputTransaction = genericLedgerTransaction
-// 	inputEnvelope := genericBumpOperationEnvelope
+func makeAssetTestInput() (inputTransaction ingest.LedgerTransaction, err error) {
+	inputTransaction = genericLedgerTransaction
+	inputEnvelope := genericBumpOperationEnvelope
 
-// 	inputEnvelope.Tx.SourceAccount = testAccount1
+	inputEnvelope.Tx.SourceAccount = testAccount1
 
-// 	inputOperations := []xdr.Operation{
-// 		xdr.Operation{
-// 			SourceAccount: nil,
-// 			Body: xdr.OperationBody{
-// 				Type: xdr.OperationTypePayment,
-// 				PaymentOp: &xdr.PaymentOp{
-// 					Destination: testAccount2,
-// 					Asset:       usdtAsset,
-// 					Amount:      350000000,
-// 				},
-// 			},
-// 		},
-// 		xdr.Operation{
-// 			SourceAccount: nil,
-// 			Body: xdr.OperationBody{
-// 				Type: xdr.OperationTypePayment,
-// 				PaymentOp: &xdr.PaymentOp{
-// 					Destination: testAccount3,
-// 					Asset:       nativeAsset,
-// 					Amount:      350000000,
-// 				},
-// 			},
-// 		},
-// 	}
+	inputOperations := []xdr.Operation{
+		xdr.Operation{
+			SourceAccount: nil,
+			Body: xdr.OperationBody{
+				Type: xdr.OperationTypePayment,
+				PaymentOp: &xdr.PaymentOp{
+					Destination: testAccount2,
+					Asset:       usdtAsset,
+					Amount:      350000000,
+				},
+			},
+		},
+		xdr.Operation{
+			SourceAccount: nil,
+			Body: xdr.OperationBody{
+				Type: xdr.OperationTypePayment,
+				PaymentOp: &xdr.PaymentOp{
+					Destination: testAccount3,
+					Asset:       nativeAsset,
+					Amount:      350000000,
+				},
+			},
+		},
+	}
 
-// 	inputEnvelope.Tx.Operations = inputOperations
-// 	inputTransaction.Envelope.V1 = &inputEnvelope
-// 	return
-// }
+	inputEnvelope.Tx.Operations = inputOperations
+	inputTransaction.Envelope.V1 = &inputEnvelope
+	return
+}
 
-// func makeAssetTestOutput() (transformedAssets []AssetOutput) {
-// 	transformedAssets = []AssetOutput{
-// 		AssetOutput{
-// 			AssetCode:   "USDT",
-// 			AssetIssuer: "GBVVRXLMNCJQW3IDDXC3X6XCH35B5Q7QXNMMFPENSOGUPQO7WO7HGZPA",
-// 			AssetType:   "credit_alphanum4",
-// 			AssetID:     1229977787683536144,
-// 			ID:          -8205667356306085451,
-// 		},
-// 		AssetOutput{
-// 			AssetCode:   "",
-// 			AssetIssuer: "",
-// 			AssetType:   "native",
-// 			AssetID:     12638146518625398189,
-// 			ID:          -5706705804583548011,
-// 		},
-// 	}
-// 	return
-// }
+func makeAssetTestOutput() (transformedAssets []AssetOutput) {
+	transformedAssets = []AssetOutput{
+		AssetOutput{
+			AssetCode:   "USDT",
+			AssetIssuer: "GBVVRXLMNCJQW3IDDXC3X6XCH35B5Q7QXNMMFPENSOGUPQO7WO7HGZPA",
+			AssetType:   "credit_alphanum4",
+			AssetID:     1229977787683536144,
+			ID:          -8205667356306085451,
+		},
+		AssetOutput{
+			AssetCode:   "",
+			AssetIssuer: "",
+			AssetType:   "native",
+			AssetID:     12638146518625398189,
+			ID:          -5706705804583548011,
+		},
+	}
+	return
+}
