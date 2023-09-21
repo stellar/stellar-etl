ecbd1d9f93e818be3c5141970a972b5111929919
Simon Chow <simon.chow@stellar.org>
2023-09-20 21:40:23 -0400

    update getclosetime

--- internal/utils/main.go
+++ internal/utils/main.go
@@ -685,14 +685,6 @@ func (e EnvironmentDetails) GetUnboundedLedgerCloseMeta(end uint32) (xdr.LedgerC
 }
 
 func GetCloseTime(lcm xdr.LedgerCloseMeta) (time.Time, error) {
-	switch lcm.V {
-	case 0:
-		return ExtractLedgerCloseTime(lcm.MustV0().LedgerHeader)
-	case 1:
-		return ExtractLedgerCloseTime(lcm.MustV1().LedgerHeader)
-	case 2:
-		return ExtractLedgerCloseTime(lcm.MustV2().LedgerHeader)
-	default:
-		panic(fmt.Sprintf("Unsupported LedgerCloseMeta.V: %d", lcm.V))
-	}
+	headerHistoryEntry := lcm.LedgerHeaderHistoryEntry()
+	return ExtractLedgerCloseTime(headerHistoryEntry)
 }
